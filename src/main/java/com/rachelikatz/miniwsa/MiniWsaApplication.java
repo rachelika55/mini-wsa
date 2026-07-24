1e9c001f864a4b5e3110da53e54132b74084bf1a
rachelika55 <racheli.katz.55@gmail.com>
2026-07-24 10:56:36 +0300

    Initialize Spring Boot project

---
+++ src/main/java/com/rachelikatz/miniwsa/MiniWsaApplication.java
@@ -0,0 +1,13 @@
+package com.rachelikatz.miniwsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniWsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rachelikatz/miniwsa/MiniWsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachelikatz.miniwsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
